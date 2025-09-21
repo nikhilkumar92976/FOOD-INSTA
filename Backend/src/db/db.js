bd6406905025c900875b00fa2927435ca459d2c1
nikhilkumar92976 <nikhilkumar92976@gmail.com>
2025-09-21 17:24:16 +0530

    some changes added

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 // console.log("DB_URL is:", process.env.DB_URL);
+// "mongodb://localhost:27017/foodinsta"
 function connectDB(){
-    mongoose.connect("mongodb://localhost:27017/foodinsta")
+    mongoose.connect(process.env.MONGOBD_URL)
     .then(() => console.log('DB Connected'))
     .catch((err) => console.log(err));
 }
